64f6d912f3b34d171cd740f3f90719f3f5b9a24e
Rahul Mody <rmody3@gmail.com>
2017-03-14 18:02:41 -0400

    removed a byebug woops

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,6 @@
 class TagsController < ApplicationController
 
   def create
-    byebug
     params[chronicle[tags]].each do |tag|
       @tag = Tag.create(name: tag)
       ChronicleTag.create(chronicle_id: parms[:chronicle_id], tag_id: @tag.id)
