cb923a488c8e68a7b7fabb769f78b6b2e101ef92
rmody3 <rmody3@gmail.com>
2017-03-14 19:59:16 -0400

    minor changes: switched article search terms to and instead of or search,made sure only uniq chronicle searches come up, add images to the articles in the chronicle show page

--- app/models/chronicle.rb
+++ app/models/chronicle.rb
@@ -12,6 +12,7 @@ class Chronicle < ApplicationRecord
   def self.search(search_term)
     chronicle_match = Chronicle.joins(:tags).where('tags.name ILIKE ? AND private = ?',  "%#{search_term}%", false)
     chronicle_match += Chronicle.where('name ILIKE ? AND private = ?', "%#{search_term}%", false)
+    chronicle_match.uniq
     # chronicle_match += Chronicle.joins(:subscriptions).where('chronicle.name ILIKE ? AND subscriptions.admin_id = ? AND private = ?', "%#{search_term}%", current_account.id, true )
     # chronicle doesnt know the current_account method..
   end
